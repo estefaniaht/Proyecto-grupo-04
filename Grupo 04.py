0890f44b114c85bca32d905dccd1d5b450040f44
estefaniaht <106989387+estefaniaht@users.noreply.github.com>
2022-06-09 11:58:26 -0500

    Add files via upload

---
+++ Grupo 04.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import urllib.request
+@st.experimental_memo
+def download_data():
+ url = 'https://files.minsa.gob.pe/s/eRqxR35ZCxrzNgr/download'
+ filename = 'data.csv'
+ urllib.request.urlretrieve(url, filename)
+download_data()
+
+
+st.title("      Casos positivos COVID-19", anchor = None)
+
+st.markdown("En la presente página se visualizará distintos gráficos con datos relacionados a la evolución de los casos postivos de COVID-19.")
+st.subheader('Equipo 04')
+st.markdown("*Sandy Castillo Mallqui, Estefania Huaman Tovar, Maria Rivera Chiclla, Jackeline Roque Maceda y Heydi Surco Mamani*")
+
+#####CONTEXTO
+st.subheader("Contexto:")
+st.markdown("textooooo")
+from PIL import Image    ######Insertar imagen en streamlit
+image = Image.open('covid.jpg')
+st.image(image)
+
+######Codigo para insertar dataframe (FALTA)
+
+option = st.selectbox(
+     'How would you like to be contacted?',
+     ('Email', 'Hola', 'Mobile phone'))
+st.write('You selected:', option)
